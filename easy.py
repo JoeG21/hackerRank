e42e551bb98938d6b503aa6e7d0f9154dad5fe0b
JoeG21 <jcggomez@live.com>
2023-12-08 00:06:09 -0500

    initial commit

---
+++ easy.py
@@ -0,0 +1,118 @@
+'''
+**************
+1: Capitalize!
+**************
+'''
+# def solve(s):
+#     x = s.split(' ')
+#     y = [word.capitalize() for word in x]
+#     z = ' '.join(y)
+
+#     return z
+#     '''
+#     .title() doesn't work for some reason 
+#     return s.title() 
+#     .title() => capitalizes the first letter of each word in a string
+#     '''
+# print(solve(s))
+
+
+
+
+'''
+************
+2: Text Wrap 
+************
+'''
+# def wrap(string, max_width):
+#     words = [string[i:i+max_width] for i in range(0, len(string), max_width)]
+#     result = '\n'.join(words)
+#     return result
+
+
+
+
+'''
+********************
+3: String Validators
+********************
+'''
+# def string_val(string):
+#     bool_state = [
+#         any(method(char) for char in string)
+#         for method in (str.isalnum, str.isalpha, str.isdigit, str.islower, str.isupper)
+#     ]
+
+#     for bool_value in bool_state:
+#         print(bool_value)
+# string_val('ab123')
+'''
+Original solution.
+More readable 
+bool_state = [False]*5
+for char in s:
+    if char.isalnum(): bool_state[0]=True
+    if char.isalpha(): bool_state[1]=True
+    if char.isdigit(): bool_state[2]=True
+    if char.islower(): bool_state[3]=True
+    if char.isupper(): bool_state[4]=True
+for bool in bool_state:
+    print(bool)
+'''
+
+
+
+
+'''
+********************
+4: Find a string
+********************
+'''
+# def count_substring(string, sub_string):
+#     res = [i for i in range(len(string)) if string.startswith(sub_string, i)]
+#     return len(res)
+
+
+
+
+'''
+********************
+5: Nested Lists
+********************
+'''
+# if __name__ == '__main__':
+#     list_name = []
+#     list_score = []
+    
+#     for _ in range(int(input())):
+#         name = input()
+#         score = float(input())
+        
+#         list_name.append(name)
+#         list_score.append(score)
+
+#     i=0
+#     records = [[n] for n in list_name]
+#     while (i<len(list_name)):
+#         records[i].append(list_score[i])
+#         i+=1
+    
+
+#     # Extract unique grades
+#     grades = set(score for name, score in records)
+
+#     # Find the second lowest grade
+#     second_lowest_grade = sorted(grades)[1]
+
+#     # Collect names of students with the second lowest grade
+#     students_with_second_lowest = sorted(name for name, score in records if score == second_lowest_grade)
+
+#     # Print each name on a new line
+#     for student in students_with_second_lowest:
+#         print(student)
+        
+        
+        
+        
+
+
